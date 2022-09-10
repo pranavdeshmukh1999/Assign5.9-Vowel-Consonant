78fa65e63dff15f42b0f15830a7ff7c3b3fb5b2a
pranavdeshmukh1999 <pranavdeshmukh182@gmail.com>
2022-09-11 03:20:44 +0530

    Added :Char is VowelOrConsonant

---
+++ src/com/VowelOrConsonant/VowelConsonant.java
@@ -0,0 +1,16 @@
+package com.VowelOrConsonant;
+
+public class VowelConsonant {
+
+	public static void main(String[] args) {
+
+		char ch = 'a';
+
+		if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'o')
+
+			System.out.println(ch + " is Vowel");
+
+		else
+			System.out.println(ch + " is consonant");
+	}
+}
